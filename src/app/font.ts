41a59f1a8257b3ab46f4dd4669635b5a0b76c7d5
leekyuhyun <leekh010502@gmail.com>
2026-02-12 00:05:42 +0900

    feat &  fix : navbar / footer  다크모드와 폰트 추가로 인한 코드 추가 및 수정

---
+++ src/app/font.ts
@@ -0,0 +1,28 @@
+import localFont from "next/font/local";
+import { Noto_Sans_KR } from "next/font/google";
+
+export const gaegu = localFont({
+  src: [
+    {
+      path: "./font/Gaegu-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./font/Gaegu-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./font/Gaegu-light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+  ],
+  variable: "--font-gaegu",
+});
+
+export const noto = Noto_Sans_KR({
+  subsets: ["latin"],
+  variable: "--font-noto",
+});
