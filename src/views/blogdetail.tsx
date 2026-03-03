baf1e55231c8c11fa4b5a6501848787d92e6b8b1
leekyuhyun <leekh010502@gmail.com>
2026-03-03 22:35:37 +0900

    fix : blogdetail 수정

--- src/views/blogdetail.tsx
+++ src/views/blogdetail.tsx
@@ -1,4 +1,5 @@
-import { MDXRemote } from "next-mdx-remote/rsc";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 import BlogDetailHeader from "@/components/blog/BlogDetailHeader";
 import { Post } from "@/lib/blog";
 
@@ -12,7 +13,7 @@ export default function BlogDetailView({ post, content }: BlogDetailViewProps) {
     <article className="max-w-3xl mx-auto px-6 py-20 font-sans">
       <BlogDetailHeader post={post} />
       <div className="prose dark:prose-invert max-w-none mt-10">
-        <MDXRemote source={content} />
+        <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
       </div>
     </article>
   );
